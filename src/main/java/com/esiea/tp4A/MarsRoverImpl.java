bf18f5bbd8594b500c99ae8c12aefa6649425d41
Ekrynox <lucas123.a17@gmail.com>
2020-02-26 09:50:39 +0100

    Add MarsRover Implementation and Test

---
+++ src/main/java/com/esiea/tp4A/MarsRoverImpl.java
@@ -0,0 +1,27 @@
+package com.esiea.tp4A;
+
+import com.esiea.tp4A.domain.MarsRover;
+import com.esiea.tp4A.domain.PlanetMap;
+import com.esiea.tp4A.domain.Position;
+
+public class MarsRoverImpl implements MarsRover {
+    @Override
+    public MarsRover initialize(Position position) {
+        return null;
+    }
+
+    @Override
+    public MarsRover updateMap(PlanetMap map) {
+        return null;
+    }
+
+    @Override
+    public MarsRover configureLaserRange(int range) {
+        return null;
+    }
+
+    @Override
+    public Position move(String command) {
+        return null;
+    }
+}
