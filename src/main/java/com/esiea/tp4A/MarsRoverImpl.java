3d381daee4d10e19330434d26594c634eaf635f3
VicNdjr <vic.ndjr@hotmail.fr>
2020-02-26 11:20:26 +0100

    Add Test for initialize function

--- src/main/java/com/esiea/tp4A/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/MarsRoverImpl.java
@@ -1,5 +1,6 @@
 package com.esiea.tp4A;
 
+import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.PlanetMap;
 import com.esiea.tp4A.domain.Position;
@@ -23,5 +24,12 @@ public class MarsRoverImpl implements MarsRover {
     @Override
     public Position move(String command) {
         return null;
+    }
+
+
+
+    private Position position = Position.of(0, 0, Direction.NORTH);
+    public Position getPosition() {
+        return position;
     }
 }
