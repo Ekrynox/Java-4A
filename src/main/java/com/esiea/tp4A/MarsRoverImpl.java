0f1ab3dfe6b99fb3a5802cfda21faed6bb34f231
Ekrynox <lucas123.a17@gmail.com>
2020-04-04 16:44:32 +0200

    Fix error with double function

--- src/main/java/com/esiea/tp4A/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/MarsRoverImpl.java
@@ -63,11 +63,4 @@ public class MarsRoverImpl implements MarsRover {
     public Position getPosition() {
         return position;
     }
-
-
-
-    private Position position = Position.of(0, 0, Direction.NORTH);
-    public Position getPosition() {
-        return position;
-    }
 }
