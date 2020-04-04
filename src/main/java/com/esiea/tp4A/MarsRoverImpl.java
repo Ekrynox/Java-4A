236645463177d68eccc6b2cc6e29894a9e7b5fca
Ekrynox <lucas123.a17@gmail.com>
2020-04-04 23:45:37 +0200

    Add Planet spherical grid with move

--- src/main/java/com/esiea/tp4A/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/MarsRoverImpl.java
@@ -25,38 +25,42 @@ public class MarsRoverImpl implements MarsRover {
 
     @Override
     public Position move(String command) {
-        int new_x = this.position.getX(); int new_y = this.position.getY(); Direction new_D = this.position.getDirection();
-        for(int i=0 ; i<command.length() ; i++){
-            switch (command.charAt(i)){
-                case 'f':
-                    if(new_D.equals(Direction.NORTH)) new_y++;
-                    if(new_D.equals(Direction.SOUTH)) new_y--;
-                    if(new_D.equals(Direction.EAST)) new_x++;
-                    if(new_D.equals(Direction.WEST)) new_x--;
-                    break;
-                case 'b':
-                    if(new_D.equals(Direction.NORTH)) new_y--;
-                    if(new_D.equals(Direction.SOUTH)) new_y++;
-                    if(new_D.equals(Direction.EAST)) new_x--;
-                    if(new_D.equals(Direction.WEST)) new_x++;
-                    break;
-                case 'l':
-                    if(new_D.equals(Direction.NORTH)) new_D = Direction.WEST;
-                    else if(new_D.equals(Direction.SOUTH)) new_D = Direction.EAST;
-                    else if(new_D.equals(Direction.EAST)) new_D = Direction.NORTH;
-                    else if(new_D.equals(Direction.WEST)) new_D = Direction.SOUTH;
-                    break;
-                case 'r':
-                    if(new_D.equals(Direction.NORTH)) new_D = Direction.EAST;
-                    else if(new_D.equals(Direction.SOUTH)) new_D = Direction.WEST;
-                    else if(new_D.equals(Direction.EAST)) new_D = Direction.SOUTH;
-                    else if(new_D.equals(Direction.WEST)) new_D = Direction.NORTH;
-                    break;
-                default :
-                    break;
+        int new_x = this.position.getX();
+        int new_y = this.position.getY();
+        Direction new_D = this.position.getDirection();
+
+        int i;
+
+        for (char c : command.toCharArray()) {
+            i = (c == 'f' ? 1 : (c == 'b' ? -1 : 0));
+
+            if (new_D.equals(Direction.NORTH)) new_y += i;
+            if (new_D.equals(Direction.SOUTH)) new_y -= i;
+            if (new_D.equals(Direction.EAST)) new_x += i;
+            if (new_D.equals(Direction.WEST)) new_x -= i;
+
+            if (c == 'l') {
+                if (new_D.equals(Direction.NORTH)) new_D = Direction.WEST;
+                else if (new_D.equals(Direction.SOUTH)) new_D = Direction.EAST;
+                else if (new_D.equals(Direction.EAST)) new_D = Direction.NORTH;
+                else if (new_D.equals(Direction.WEST)) new_D = Direction.SOUTH;
             }
+            if (c == 'r') {
+                if (new_D.equals(Direction.NORTH)) new_D = Direction.EAST;
+                else if (new_D.equals(Direction.SOUTH)) new_D = Direction.WEST;
+                else if (new_D.equals(Direction.EAST)) new_D = Direction.SOUTH;
+                else if (new_D.equals(Direction.WEST)) new_D = Direction.NORTH;
+            }
+
+            if (new_y == 51) new_y = -49;
+            if (new_x == 51) new_x = -49;
+            if (new_y == -50) new_y = 50;
+            if (new_x == -50) new_y = 50;
+
         }
-        return Position.of(new_x,new_y,new_D);
+
+        this.position = Position.of(new_x, new_y, new_D);
+        return this.position;
     }
 
 
