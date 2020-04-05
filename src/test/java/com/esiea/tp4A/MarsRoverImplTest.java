7f32a3b5acbe0a9b78f239458854ce43132515fb
Ekrynox <lucas123.a17@gmail.com>
2020-04-05 13:07:08 +0200

    Use returned Rover for test

--- src/test/java/com/esiea/tp4A/MarsRoverImplTest.java
+++ src/test/java/com/esiea/tp4A/MarsRoverImplTest.java
@@ -1,6 +1,7 @@
 package com.esiea.tp4A;
 
 import com.esiea.tp4A.domain.Direction;
+import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,11 +19,11 @@ class MarsRoverImplTest {
     @CsvSource({"0, 0, NORTH", "2, 29, SOUTH", "4, 6, EAST", "7, 16, WEST", "-9, 19, NORTH"})
     void initialize(int x, int y, Direction dir) throws NoSuchFieldException, IllegalAccessException {
         MarsRoverImpl marsRover = new MarsRoverImpl();
-        marsRover.initialize(Position.of(x, y, dir));
+        MarsRover newMarsRover = marsRover.initialize(Position.of(x, y, dir));
 
         final Field field = marsRover.getClass().getDeclaredField("position");
         field.setAccessible(true);
-        Position position = (Position)field.get(marsRover);
+        Position position = (Position)field.get(newMarsRover);
 
         assertEquals(x, position.getX());
         assertEquals(y, position.getY());
@@ -34,8 +35,9 @@ class MarsRoverImplTest {
     @CsvSource({"'fflb', -10, 0, -9, 2, WEST", "'f', 5, 0, 5, 1, NORTH", "'rr', 0, 0, 0, 0, SOUTH", "'lfb', 0, 0, 0, 0, WEST", "'f', 0, 50, 0, -49, NORTH", "'b', 0, -49, 0, 50, NORTH", "'rf', 50, 0, -49, 0, EAST"})
     void move(String command, int posX, int posY, int x, int y, Direction dir) {
         MarsRoverImpl marsRover = new MarsRoverImpl();
-        marsRover.initialize(Position.of(posX, posY, Direction.NORTH));
-        Position position = marsRover.move(command);
+        MarsRover newMarsRover = marsRover.initialize(Position.of(posX, posY, Direction.NORTH));
+
+        Position position = newMarsRover.move(command);
         assertEquals(x, position.getX());
         assertEquals(y, position.getY());
         assertEquals(dir, position.getDirection());
@@ -46,11 +48,11 @@ class MarsRoverImplTest {
     @CsvSource({"0", "1", "30", "100", "-1"})
     void configureLaserRange(int range) throws NoSuchFieldException, IllegalAccessException {
         MarsRoverImpl marsRover = new MarsRoverImpl();
-        marsRover.configureLaserRange(range);
+        MarsRover newMarsRover = marsRover.configureLaserRange(range);
 
         final Field field = marsRover.getClass().getDeclaredField("laserRange");
         field.setAccessible(true);
 
-        assertEquals(range, field.get(marsRover));
+        assertEquals(range, field.get(newMarsRover));
     }
 }
