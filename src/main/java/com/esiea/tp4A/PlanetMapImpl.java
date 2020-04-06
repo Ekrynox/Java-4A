bcf99d22bfcfa12d1535d69b3b996baf1af13add
Ekrynox <lucas123.a17@gmail.com>
2020-04-06 14:27:43 +0200

    Add another constructor to PlanetMapImpl to choose the size and the number of obstacle

--- src/main/java/com/esiea/tp4A/PlanetMapImpl.java
+++ src/main/java/com/esiea/tp4A/PlanetMapImpl.java
@@ -11,30 +11,43 @@ import static java.lang.Math.round;
 public class PlanetMapImpl implements PlanetMap {
 
     private final int [][] map;
-    private final int size = 100;
+    private final int size;
 
-    public PlanetMapImpl() {
-        this.map = new int[this.size][this.size];
-        int x, y;
-
-        for (y = 0; y < this.size; y++) {
-            for (x = 0; x < this.size; x++) {
+    private void clearMap() {
+        for (int y = 0; y < this.size; y++) {
+            for (int x = 0; x < this.size; x++) {
                 this.map[y][x] = 0;
             }
         }
+    }
 
-        int obstaclesToSpawn = (int) round(0.15 * this.size * this.size);
-        for (int n = 0; n < obstaclesToSpawn; n++) {
-            while (true) {
-                x = (int) (Math.random() * this.size);
-                y = (int) (Math.random() * this.size);
+    private void randomObstacle() {
+        int x, y;
+        while (true) {
+            x = (int) (Math.random() * this.size);
+            y = (int) (Math.random() * this.size);
 
-                if (this.map[y][x] == 0) {
-                    this.map[y][x] = 1;
-                    break;
-                }
+            if (this.map[y][x] == 0) {
+                this.map[y][x] = 1;
+                break;
             }
         }
+    }
+
+    public PlanetMapImpl(int size, int nbObstacle) {
+        this.size = size;
+        this.map = new int[this.size][this.size];
+
+        clearMap();
+
+        int obstaclesToSpawn = Math.min(nbObstacle, size * size);
+        for (int n = 0; n < obstaclesToSpawn; n++) {
+            randomObstacle();
+        }
+    }
+
+    public PlanetMapImpl() {
+        this(100, 0);
     }
 
     @Override
