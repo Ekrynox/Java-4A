607329744fec4516454991d606c6cf754d2e30c4
AntonLsp <lesparre@et.esiea.fr>
2020-04-07 13:32:48 +0200

    Edit code to prepare testing for PlayerImpl

---
+++ src/test/java/com/esiea/tp4AGame/PlayerImplTest.java
@@ -0,0 +1,28 @@
+package com.esiea.tp4AGame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerImplTest {
+
+    @Test
+    void getRoverPosition() {
+    }
+
+    @Test
+    void radar() {
+    }
+
+    @Test
+    void getLaserRange() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void isAlive() {
+    }
+}
