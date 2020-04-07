022c7511cb85b8a8ca761dd77ed2a0c06099043e
Thomas <dognon@et.esiea.fr>
2020-04-07 13:45:40 +0200

    Test Implementation of a Player

---
+++ src/test/java/com/esiea/tp4A/PlayerImplTest.java
@@ -0,0 +1,37 @@
+package com.esiea.tp4A;
+
+public class PlayerImplTest {
+    private String name = "Spadabi";
+    private double health = 50;
+    private double range = 3.5;
+
+    PlayerImplTest(){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHealth() {
+        return health;
+
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public double getRange() {
+        return range;
+    }
+
+    public void setRange(double range) {
+        this.range = range;
+    }
+}
