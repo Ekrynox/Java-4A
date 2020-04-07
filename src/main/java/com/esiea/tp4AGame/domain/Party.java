ae7e8aa88cc7a5b6ed2386c91d683e7c210bdc60
TomaDog <47032498+TomaDog@users.noreply.github.com>
2020-04-07 13:03:10 +0200

    Add javadoc to interface

--- src/main/java/com/esiea/tp4AGame/domain/Party.java
+++ src/main/java/com/esiea/tp4AGame/domain/Party.java
@@ -1,10 +1,26 @@
 package com.esiea.tp4AGame.domain;
 
+import java.util.Set;
+
 public interface Party {
+    /** Create a new player with a name, if the name is not taken
+     * @param playerName The name of the player in the game
+     * @return An object to control the player
+     */
     Player addPlayer(String playerName);
 
+    /** Start the party if there is at least 2 players
+     * @return true if the party is started
+     */
     boolean startParty();
 
-    boolean isFinish();
+    /** Get the name of all alive players
+     * @return A list of the name
+     */
+    Set<String> getAlivePlayers();
+
+    /** Get the state of the game
+     * @return the name of the winner, if empty string the game is not finished
+     */
     String getWinner();
 }
