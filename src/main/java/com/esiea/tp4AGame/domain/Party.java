57365e7766b4f785897c45d0b6d35ef90fd667a6
TomaDog <47032498+TomaDog@users.noreply.github.com>
2020-04-07 12:47:37 +0200

    Move the party interface to player and create a new party interface

--- src/main/java/com/esiea/tp4AGame/domain/Party.java
+++ src/main/java/com/esiea/tp4AGame/domain/Party.java
@@ -1,23 +1,10 @@
 package com.esiea.tp4AGame.domain;
 
-import com.esiea.tp4A.domain.Position;
-
-import java.util.Set;
-
 public interface Party {
-    Position getRoverPosition();
+    Player addPlayer(String playerName);
 
-    Set<Position> radar(int radarSize); // Obstacle have a null Direction
-    Set<Position> radar(); // radarObstacle(30);
+    boolean startParty();
 
-    int getLaserRange();
-
-    Position goForward();
-    Position goBackward();
-    Position turnRight();
-    Position turnLeft();
-
-    boolean shoot();
-
-    boolean isAlive();
+    boolean isFinish();
+    String getWinner();
 }
