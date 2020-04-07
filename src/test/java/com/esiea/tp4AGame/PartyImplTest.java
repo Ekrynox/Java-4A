7fc3cec99cf664d86b04469d28cc9a3e335f3e0b
VicNdjr <vic.ndjr@hotmail.fr>
2020-04-07 16:41:28 +0200

    Create three minimal test to start Party

---
+++ src/test/java/com/esiea/tp4AGame/PartyImplTest.java
@@ -0,0 +1,41 @@
+package com.esiea.tp4AGame;
+
+import com.esiea.tp4AGame.domain.Party;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartyImplTest {
+    @Test
+    void addPlayer() {
+        Party party1 = new PartyImpl();
+        assertNotNull(party1.addPlayer("a"));
+        assertNull(party1.addPlayer("a"));
+    }
+
+    @Test
+    void start() {
+        Party party1 = new PartyImpl();
+        assertFalse(party1.start());
+
+        party1.addPlayer("a");
+        assertFalse(party1.start());
+
+        party1.addPlayer("aa");
+        assertTrue(party1.start());
+    }
+
+    @Test
+    void isStarted() {
+        Party party1 = new PartyImpl();
+        assertFalse(party1.isStarted());
+
+        party1.addPlayer("a");
+        assertFalse(party1.isStarted());
+        party1.addPlayer("aa");
+        assertFalse(party1.isStarted());
+
+        party1.start();
+        assertTrue(party1.isStarted());
+    }
+}
