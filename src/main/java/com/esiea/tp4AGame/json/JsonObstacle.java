424f3cc9f820da439377a79802266fce7e341349
Damien Gounot <gounot@et.esiea.fr>
2020-04-11 16:40:24 +0200

    Add Gson to Maven and Class for Json return for API

---
+++ src/main/java/com/esiea/tp4AGame/json/JsonObstacle.java
@@ -0,0 +1,11 @@
+package com.esiea.tp4AGame.json;
+
+public class JsonObstacle {
+    public JsonObstacle(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public final int x;
+    public final int y;
+}
---
+++ src/main/java/com/esiea/tp4AGame/json/JsonPlayer.java
@@ -0,0 +1,37 @@
+package com.esiea.tp4AGame.json;
+
+import com.esiea.tp4A.domain.Position;
+import com.google.gson.annotations.SerializedName;
+
+public class JsonPlayer {
+    public JsonPlayer(String name, boolean isAlive, Position position, int laser_range) {
+        this.name = name;
+        this.status = isAlive ? "alive" : "dead";
+
+        String direction = "";
+        switch (position.getDirection()) {
+            case NORTH:
+                direction = "NORTH";
+                break;
+            case SOUTH:
+                direction = "SOUTH";
+                break;
+            case EAST:
+                direction = "EAST";
+                break;
+            case WEST:
+                direction = "WEST";
+                break;
+        }
+        this.position = new JsonPosition(position.getX(), position.getY(), direction);
+
+        this.laser_range = laser_range;
+    }
+
+    public final String name;
+    public final String status;
+    public final JsonPosition position;
+
+    @SerializedName(value = "laser-range")
+    public final int laser_range;
+}
