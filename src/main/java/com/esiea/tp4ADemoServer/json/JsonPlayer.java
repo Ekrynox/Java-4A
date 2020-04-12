7f4e152d6f55e6b615d9f50d58adc2955f4497b3
Damien Gounot <gounot@et.esiea.fr>
2020-04-12 18:37:14 +0200

    Try to Fix Json conversion coverage

--- src/main/java/com/esiea/tp4ADemoServer/json/JsonPlayer.java
+++ src/main/java/com/esiea/tp4ADemoServer/json/JsonPlayer.java
@@ -1,5 +1,6 @@
 package com.esiea.tp4ADemoServer.json;
 
+import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.Position;
 import com.google.gson.annotations.SerializedName;
 
@@ -9,19 +10,15 @@ public class JsonPlayer {
         this.status = isAlive ? "alive" : "dead";
 
         String direction = "";
-        switch (position.getDirection()) {
-            case NORTH:
-                direction = "NORTH";
-                break;
-            case SOUTH:
-                direction = "SOUTH";
-                break;
-            case EAST:
-                direction = "EAST";
-                break;
-            case WEST:
-                direction = "WEST";
-                break;
+        Direction positionDirection = position.getDirection();
+        if (positionDirection == Direction.NORTH) {
+            direction = "NORTH";
+        } else if (positionDirection == Direction.SOUTH) {
+            direction = "SOUTH";
+        } else if (positionDirection == Direction.EAST) {
+            direction = "EAST";
+        } else if (positionDirection == Direction.WEST) {
+            direction = "WEST";
         }
         this.position = new JsonPosition(position.getX(), position.getY(), direction);
 
